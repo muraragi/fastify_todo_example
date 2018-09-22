2c060a0fe96a25eebdaaf11fe653efa0fe04f1c4
Marat <muraragi99@gmail.com>
2018-09-22 17:23:31 +0300

    added serialization and validation

--- routes/todo.js
+++ routes/todo.js
@@ -1,21 +1,85 @@
 async function routes(fastify, options) {
   const Todo = fastify.todoModel
 
+  //Serialization options
+  const getAllOpts = {
+    schema: {
+      description: 'Get all todos',
+      response: {
+        200: {
+          type: 'array',
+          items: {
+            type: 'object',
+            properties: {
+              completed: { type: 'boolean' },
+              _id: { type: 'string' },
+              name: { type: 'string' },
+              created_date: { type: 'string', format: 'date-time' },
+              __v: { type: 'number' },
+            },
+          },
+        },
+      },
+    },
+  }
+
+  const getOneOpts ={
+    schema: {
+      description: 'Get all todos',
+      response: {
+        200: {
+          type: 'object',
+          properties: {
+            completed: { type: 'boolean' },
+            _id: { type: 'string' },
+            name: { type: 'string' },
+            created_date: { type: 'string', format: 'date-time' },
+            __v: { type: 'number' },
+          },
+        },
+      },
+    },
+  }
+
+  //Validation Opts
+  const createOpts = {
+    schema: {
+      body: {
+        type: 'object',
+        properties: {
+          name: { type: 'string', minLength: 2 },
+        },
+      },
+    },
+  }
+  
+  const updateOpts = {
+    schema: {
+      body: {
+        type: 'object',
+        properties: {
+          name: { type: 'string', minLength: 2 },
+          completed: {type: 'boolean'}
+        },
+      },
+    },
+  }
+
   //All Todos
-  fastify.get('/', async (request, reply) => {
+  fastify.get('/', getAllOpts, async (request, reply) => {
     const result = await Todo.find({})
     return result
   })
 
   //One Todo
-  fastify.get('/:id', async (requset, reply) => {
+  fastify.get('/:id', getOneOpts, async (requset, reply) => {
     const id = requset.params.id
     const result = await Todo.findById(id)
     return result
   })
 
   //New Todo
-  fastify.post('/', async (request, reply) => {
+  fastify.post('/', createOpts, async (request, reply) => {
     const todo = request.body
 
     await Todo.create(todo)
@@ -34,7 +98,7 @@ async function routes(fastify, options) {
 
   //Update Todo
 
-  fastify.put('/:id', async (request, reply) => {
+  fastify.put('/:id', updateOpts, async (request, reply) => {
     const id = request.params.id
     const updatedTodo = request.body
 

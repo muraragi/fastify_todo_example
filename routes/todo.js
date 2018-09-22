ffd8d68a74ab0c57078a64d81caef077ad252f91
Marat <muraragi99@gmail.com>
2018-09-22 15:55:46 +0300

    implemented Delete route

--- routes/todo.js
+++ routes/todo.js
@@ -1,23 +1,35 @@
 async function routes(fastify, options) {
   const Todo = fastify.todoModel
 
+  //All Todos
   fastify.get('/', async (request, reply) => {
     const result = await Todo.find({})
     return result
   })
 
+  //One Todo
   fastify.get('/:id', async (requset, reply) => {
     const id = requset.params.id
     const result = await Todo.findById(id)
     return result
   })
 
+  //New Todo
   fastify.post('/', async (request, reply) => {
     const todo = request.body
 
     await Todo.create(todo)
     reply.code(201)
-    return {success: true, id: todo.id}
+    return { success: true, id: todo.id }
+  })
+
+  //Delete Todo
+  fastify.delete('/:id', async (request, reply) => {
+    const id = request.params.id
+
+    await Todo.findByIdAndRemove(id)
+    reply.code(201)
+    return { success: true, id }
   })
 }
 
